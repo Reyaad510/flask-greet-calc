b7fe0bb1036e3bd011ab34a1787cacee567d6e23
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-03-02 14:12:15 -0500

    Exercise completed

--- calc/app.py
+++ calc/app.py
@@ -1,4 +1,45 @@
 # Put your app in here.
-from flask import Flask
+from flask import Flask, request
+from operations import add, sub, mult, div
 
 app = Flask(__name__)
+
+
+@app.route("/add")
+def add_nums():
+    """ Add numbers """
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    
+    return str(add(a,b))
+
+
+@app.route("/sub")
+def sub_nums():
+    """ Subtract numbers """
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    
+    return str(sub(a,b))
+
+
+
+@app.route("/mult")
+def mult_nums():
+    """ Multiply numbers """
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    
+    return str(mult(a,b))
+
+
+
+@app.route("div")
+def div_nums():
+    """ Divide numbers """
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    
+    return str(div(a,b))
+
+  
